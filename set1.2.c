6e00d21c671298e5f356b8cc183ed526c85c1451
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-02-18 16:21:54 +0530

    Create set1.2.c

---
+++ set1.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    printf("enter the number");
+    scanf("%d",&r);
+    if(r%2==0)
+    {
+        printf("even");
+    }
+    else 
+    {
+        printf("invalid");
+        
+    }
+
+    return 0;
+}
