26f55865b486b9a450c990449462b7e27656e6b1
PAVITHRAVINI <47105366+PAVITHRAVINI@users.noreply.github.com>
2019-02-18 16:05:47 +0530

    Create set1-1.c

---
+++ set1-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a>0)
+    {
+    printf("a is positive");
+    }
+    else
+    {
+        printf("a is negative");
+    }
+
+    return 0;
+}
